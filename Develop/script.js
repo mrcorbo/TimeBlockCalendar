61d3e88b8cb42b75c6bc850c6b2cd262da7312a9
mrcorbo <109931379+mrcorbo@users.noreply.github.com>
2023-01-09 10:24:55 -0700

    updated html to run script.js

--- Develop/script.js
+++ Develop/script.js
@@ -32,6 +32,18 @@ $(document).ready(function () {
 
     }
     hourGrabber()
-
     
+    // pull all hours from 8-5 from localStorage
+    $('#hour-8 .description').val(localStorage.getItem('hour-8'))
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'))
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'))
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'))
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'))
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'))
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'))
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'))
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'))
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'))
+    // displays today on the page
+    $('#currentDay').text(moment().format('dddd, MMMM, Do'))
 })
